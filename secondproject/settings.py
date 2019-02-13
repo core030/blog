2fb69241302eb46108a84f6fe6f1a915f679b330
Sojung <ssjsuave@gmail.com>
2019-02-14 03:22:10 +0900

    heroku again

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ps1f31^y2*+t7b4l13xxs%sye)%4*6
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
